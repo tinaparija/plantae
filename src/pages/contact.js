1c6cd9034fbea0de9c0a58c72f4ada89a46e1be7
tinaparija <tinaparija@gmail.com>
2018-05-18 22:38:11 -0700

    contact updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -64,7 +64,7 @@ class Contact extends Component {
         <div className ="form-container col-sm-6 offset-sm-3">
           <div className ="col-sm-12 contact-form">
           <h3>Order Request</h3> 
-          <p className="col-sm-8 offset-sm-2">Please fill out the form below. We will coordinate a delivery with you shortly.</p>
+          <p className="col-sm-8 offset-sm-2">Please fill out the form below and we will coordinate a delivery with you shortly.</p>
           </div> 
           <form className = "col-sm-8 offset-sm-2" onSubmit={this.postForm}>
             <div className="form-group">
